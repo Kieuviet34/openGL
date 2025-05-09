65636d64b49f2c3e5e225b188e341c92caee4561
Kieuviet34 <kieuviet34@gmail.com>
2025-05-09 11:42:46 +0700

    add camera control class

---
+++ ultis/camera.h
@@ -0,0 +1,16 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "lib/glad.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+enum movementDirection{
+    
+};
+
+class Camera{
+    
+};
+
+#endif
