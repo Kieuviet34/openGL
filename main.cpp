a1948e4120e8625fab84d8094c5d60da36d4977c
Kieuviet34 <kieuviet34@gmail.com>
2025-05-14 00:55:47 +0700

    test create window

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@
 #include "ultis/shaderReader.h"
 #include "ultis/camera.h"
 
+const int WIDTH = 1080;
+const int HEIGHT = 1920;
+
 void processInput(GLFWwindow * window);
 void framebuffer_size_callback(GLFWwindow * window, int width, int height);
 void mouse_callback(GLFWwindow * window, double xpos, double ypos);
@@ -16,6 +19,24 @@ void scroll_callback(GLFWwindow * window, double xoffset, double yoffset);
     
 int main(){
     
+    glfwInit();
 
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Map generated tool", NULL, NULL);
+    if(window == NULL){
+        glfwTerminate();
+        return -1;
+    }
+
+    
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        return -1;
+    }
+
+
+    while(!glfwWindowShouldClose(window)){
+
+
+
+    }
     return 0;
 }
